67f102832ffc758ca8de1848c0c3791537543058
AndryRio <andreisprincenatu2@gmail.com>
2024-04-29 16:32:16 +0200

    fixing date in the exel file (not working)

--- app/Exports/ContactExport.php
+++ app/Exports/ContactExport.php
@@ -4,15 +4,16 @@ namespace App\Exports;
 
 use App\Contact;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ContactExport implements FromCollection
+class ContactExport implements FromCollection, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-      //return Contact::all();
-        return Contact::get(array('name','number'));
+        return Contact::get(['name', 'number', Date::dateTimeToExcel('created_at')]);
     }
 }
